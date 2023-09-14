60af2698a08d40f7dbe721b01dbb6801b986683a
Raj Laud <raj.laud@gmail.com>
2023-09-14 13:25:59 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="victron-ble-ha-parser",
+    version="0.1.0",
+    license="apache-2.0",
+    author="Raj Laud",
+    author_email="raj.laud@gmail.com",
+    description="A parser for Victron BLE messages suitable for use with Home Assistant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rajlaud/victron-ble-ha-parser",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "bluetooth-sensor-state-data>=1.6.1",
+        "sensor-state-data>=2.16.0",
+        "victron-ble>=0.6.0",
+    ],
+)
