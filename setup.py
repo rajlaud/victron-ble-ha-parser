476f0db1a936a0239319a20905bcfbcb2cda3391
Raj Laud <raj.laud@gmail.com>
2024-10-19 09:37:39 -0400

    Bump victron-ble to 0.9.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="victron_ble_ha_parser",
-    version="0.4.0",
+    version="0.4.1",
     license="apache-2.0",
     author="Raj Laud",
     author_email="raj.laud@gmail.com",
@@ -18,6 +18,6 @@ setuptools.setup(
     install_requires=[
         "bluetooth-sensor-state-data>=1.6.1",
         "sensor-state-data>=2.16.0",
-        "victron-ble @ git+https://github.com/rajlaud/victron-ble.git",
+        "victron-ble>=0.9.0",
     ],
 )
