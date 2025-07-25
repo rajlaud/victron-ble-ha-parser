c45232da429437f001f98fb727d8d5748701544e
Raj Laud <raj.laud@gmail.com>
2025-07-25 13:37:16 +0000

    Expose enums to HA

--- victron_ble_ha_parser/__init__.py
+++ victron_ble_ha_parser/__init__.py
@@ -2,10 +2,24 @@
 
 from .custom_state_data import SensorDeviceClass, Units, Keys
 from .parser import VictronBluetoothDeviceData
+from victron_ble.devices.base import (
+    OperationMode,
+    ChargerError,
+    AlarmReason,
+)
+from victron_ble.devices.battery_monitor import AuxMode
+from victron_ble.devices.smart_lithium import BalancerStatus
+from victron_ble.devices.dc_energy_meter import MeterType
 
 __all__ = [
     "Keys",
     "Units",
     "SensorDeviceClass",
     "VictronBluetoothDeviceData",
+    "OperationMode",
+    "ChargerError",
+    "AuxMode",
+    "AlarmReason",
+    "MeterType",
+    "BalancerStatus",
 ]
